142f864d8cbcd10b15087756be119de698b3c953
priyankamohite <priyankam@weboniselab.com>
2013-05-20 11:03:52 +0530

    first commit

---
+++ myClass.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: webonise
+ * Date: 20/5/13
+ * Time: 10:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MyClss{
+
+}
