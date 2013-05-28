d90a94fefce7167286d1aee409531a5253242a64
priyankamohite <priyankam@weboniselab.com>
2013-05-28 18:50:24 +0530

    where method written

--- index.php
+++ index.php
@@ -1,98 +1,99 @@
 <?php
 
-class MyDBClass{
+class MyDBClass {
 
     private static $instance;
 
-    private function __construct(){
+    private function __construct() {
 
     }
 
-    public static function getInstance(){
-        if(!self::$instance){
-
+    public static function getInstance() {
+        if (!self::$instance) {
             self::$instance = new MyDBClass();
         }
-
         return self::$instance;
     }
 
-    function makeConnection()
-    {
+    function makeConnection() {
         $hostname = 'localhost';
         $username = 'root';
         $password = 'webonise6186';
 
         try {
             $dbh = new PDO("mysql:host=$hostname;dbname=test", $username, $password);
-                        echo 'Connected to database<br />';
+//            echo 'Connected to database<br />';
             return $dbh;
-        }
-
-        catch(PDOException $e)
-        {
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 
-    function closeConnection()
-    {
-        $dbh=null;
-                echo "Connection closed<br />";
+    function closeConnection() {
+        $dbh = null;
+//        echo "Connection closed<br />";
     }
 
-    function select($select){
+    function select($select) {
 
-        if(empty($select) || $select==NULL){
+        if (empty($select) || $select == NULL) {
 
             $select = "*";
         }
-
         return $select;
-
     }
 
-    function from($from){
-
+    function from($from) {
         return $from;
+    }
 
+    function where($conditions){
+
+        $wherecondition =NULL;
+
+        foreach($conditions as $key=>$condition){
+
+            $wherecondition =  $wherecondition." ".$key ."='".$condition."' AND ";
+        }
+
+        return substr($wherecondition, 0, -4);
     }
 
 
-    function fetchdata($dbh,$mode)
-    {
+    function fetchdata($dbh) {
 
-        $queryStatement = "select ". MyDBClass::select("*"). " from ".MyDBClass::from("users").";";
+        $conditions= array('fname'=>'fname9','city'=>'City3');
+
+        $queryStatement = "select " . MyDBClass::select("*") . " from " . MyDBClass::from("users") ." where ".MyDBClass::where($conditions). ";";
+
+//        echo $queryStatement;
 
         $sth = $dbh->prepare($queryStatement);
         $sth->execute();
 
-        $results = $sth->fetchAll($mode);
+        $results = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-        if(!empty($results)){
+        if (!empty($results)) {
 
-            foreach($results as $result)
-            {
-//                                echo $result['id']."    ".$result['organisation_id']."    ".$result['fname']."    ".$result['lname']."  ".$result['city']."<br/>";
-                echo $result['fname']."<br/>";
+            foreach ($results as $result) {
+
+                if(isset($result['organisation_id'])){echo $result['organisation_id']." ";}
+                if(isset($result['fname'])){echo  $result['fname']."    ";}
+                if(isset($result['lname'])){echo $result['lname']." ";}
+                if(isset($result['city'])){echo $result['city']."   ";}
+                echo "<br />";
             }
 
-            print("\n");
-
-        }else{
+        } else {
             echo "data not found";
-            print("\n");
         }
-
-
     }
 
 }
 
-$obj=MyDBClass::getInstance();
-$dbh= $obj->makeConnection();
+$obj = MyDBClass::getInstance();
+$dbh = $obj->makeConnection();
 
-$mode=PDO::FETCH_ASSOC;
-$obj->fetchdata($dbh,$mode);
+$obj->fetchdata($dbh);
 
 $obj->closeConnection();
