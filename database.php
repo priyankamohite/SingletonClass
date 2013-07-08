59df7b53aca75f5404729508d963f52613d6f1de
priyankamohite <priyankam@weboniselab.com>
2013-07-08 19:14:20 +0530

    written script

---
+++ database.php
@@ -0,0 +1,42 @@
+<?php
+$hostname = 'localhost';
+$username = 'root';
+$password = 'webonise6186';
+$dbname = 'test';
+
+$dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+echo 'Connected to database<br />';
+
+$sql = "SHOW TABLES FROM $dbname";
+$result = mysql_query($sql);
+
+while ($rows = mysql_fetch_row($result)) {
+
+  foreach($rows as $row){
+    print_r($row);
+/*
+      $result_one = mysql_query("SHOW COLUMNS FROM $row");
+      if (mysql_num_rows($result_one) > 0) {
+          while ($row_one = mysql_fetch_assoc($result_one)) {
+              print_r($row_one);
+          }
+      }*/
+
+
+      $query_one = 'DESCRIBE users';
+      $results_one = mysql_query($query_one);
+
+      while($row_one = mysql_fetch_array($results_one)) {
+          print_r($row_one);
+          echo '<br />';
+      }
+
+
+  }
+    echo "<br />";
+}
+
+$dbh = null;
+echo '<br/><br/>Connection closed';
+
+//http://www.php.net/manual/en/function.mysql-list-tables.php
